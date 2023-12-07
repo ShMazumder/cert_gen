8a403f14aec508d441efefd5d5b2f0a805ddf393
Shazzad Hossain Mazumder <shmazumder23@gmail.com>
2023-12-07 17:04:03 +0600

    Update email.php

--- email.php
+++ email.php
@@ -34,8 +34,8 @@ $body .= chunk_split(base64_encode($message));
 
 //attachment
 $body .= "--$boundary\r\n";
-$body .= "Content-Type: multipart/mixed; name=" . $name . "\r\n";
-$body .= "Content-Disposition: attachment; filename=" . $name . "\r\n";
+$body .= "Content-Type: multipart/mixed; name=" . "certificate.png" . "\r\n";
+$body .= "Content-Disposition: attachment; filename=" . "certificate.png" . "\r\n";
 $body .= "Content-Transfer-Encoding: base64\r\n";
 $body .= "X-Attachment-Id: " . rand(1000, 99999) . "\r\n\r\n";
 $body .= $encoded_content; // Attaching the encoded file with email
